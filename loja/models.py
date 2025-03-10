0eed96c155dee1c204bb3e5bb24cf4de7e46fae2
rArtistaa <gabriellsbagetti@gmail.com>
2025-03-10 15:36:21 -0300

    algumas mudanças

--- loja/models.py
+++ loja/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
 class Produto(models.Model):
-    nome = models.CharField(max_length=200)
+    nome = models.CharField(max_length=255)
     descricao = models.TextField()
+    lancamento = models.DateField(null=True, blank=True)
+    desenvolvedor = models.CharField(max_length=155, null=True, blank=True)
+    distribuidor = models.CharField(max_length=155, null=True, blank=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    imagem = models.ImageField(upload_to='produtos/')
+    imagem = models.ImageField(upload_to='produtos/', null=True, blank=True)
+    categoria = models.ManyToManyField(Categoria, related_name='produtos', null=True)
 
     def __str__(self):
         return self.nome
--- loja/views.py
+++ loja/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from .models import Produto
+from .models import Produto, Categoria
 
 def home(request):
     produtos = Produto.objects.all()
-    return render(request, 'loja/home.html', {'produtos': produtos})
+    categorias = Categoria.objects.all()
+    return render(request, 'loja/home.html', {'produtos': produtos, 'categorias': categorias})
+
+def lista_produtos(request):
+    produtos = Produto.objects.all()
+
+    categoria_id = request.GET.get('categoria')
+    if categoria_id:
+        produtos = produtos.filter(categoria_id=categoria_id)
+    
+    return render(request, 'loja/produtos.html', {'produtos': produtos})
