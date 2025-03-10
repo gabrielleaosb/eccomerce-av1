a01818277cd7f624ddb53711efb06df29e82c385
rArtistaa <gabriellsbagetti@gmail.com>
2025-03-10 20:42:21 -0300

    atualização do codigo

--- loja/admin.py
+++ loja/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Produto, Categoria
+from .models import Produto, Categoria, Plataforma, Sistema, Tipo, Distribuidor
 
 admin.site.register(Produto)
+admin.site.register(Plataforma)
+admin.site.register(Sistema)
+admin.site.register(Tipo)
 admin.site.register(Categoria)
+admin.site.register(Distribuidor)
--- loja/models.py
+++ loja/models.py
@@ -7,15 +7,42 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nome
 
+class Plataforma(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Sistema(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Tipo(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+    
+class Distribuidor(models.Model):
+    nome = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.nome
+    
 class Produto(models.Model):
     nome = models.CharField(max_length=255)
     descricao = models.TextField()
     lancamento = models.DateField(null=True, blank=True)
     desenvolvedor = models.CharField(max_length=155, null=True, blank=True)
-    distribuidor = models.CharField(max_length=155, null=True, blank=True)
+    distribuidor =  models.ForeignKey(Distribuidor, on_delete=models.SET_NULL, null=True, blank=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     imagem = models.ImageField(upload_to='produtos/', null=True, blank=True)
-    categoria = models.ManyToManyField(Categoria, related_name='produtos', null=True)
+    categoria = models.ManyToManyField(Categoria, related_name='produtos')
+    plataforma = models.ManyToManyField(Plataforma, related_name='produtos')
+    sistema = models.ManyToManyField(Sistema, related_name='produtos')
+    tipo = models.ManyToManyField(Tipo, related_name='produtos')
 
     def __str__(self):
         return self.nome
